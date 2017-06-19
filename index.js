868f871dd4f9767e4ea7ff1d24bc66eaa4568072
mamaz <hisma.sudradjat@btpn.com>
2017-06-19 16:34:23 +0700

    Add doc headers

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { NativeModules } from 'react-native';
 const RNReactNativeDocViewer = NativeModules.RNReactNativeDocViewer;
 export default {
   openDoc: RNReactNativeDocViewer.openDoc,
+  openDocHeaders: RNReactNativeDocViewer.openDocHeaders,
   openDocb64: RNReactNativeDocViewer.openDocb64,
   playMovie: RNReactNativeDocViewer.playMovie,
   openDocBinaryinUrl:RNReactNativeDocViewer.openDocBinaryinUrl,
